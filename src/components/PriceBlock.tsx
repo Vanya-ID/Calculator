fb3c35d412ec5c2237fd812a2f7a6ab711ec2fe9
Vanya-afk <wot.vania@mail.ru>
2022-01-02 22:12:47 +0300

    feat(modal): change width

--- src/components/PriceBlock.tsx
+++ src/components/PriceBlock.tsx
@@ -53,7 +53,7 @@ export const PriceBlock = () => {
                     top: '50%',
                     left: '50%',
                     transform: 'translate(-50%, -50%)',
-                    width: 400,
+                    minWidth: 200,
                     bgcolor: 'background.paper',
                     border: '2px solid #000',
                     boxShadow: 24,
